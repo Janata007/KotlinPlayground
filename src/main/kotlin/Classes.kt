d0bc701e55a583161fff7724d26573d4c3f81611
Jana Markovikj <j.markovic@artemisoft.mk>
2021-09-30 15:52:44 +0200

    additions

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,22 +1,22 @@
-import javax.naming.Context
-
 class Rectangle(var height: Double, var length: Double) {
     var perimeter = (height + length) * 2
 }
 
-open class Person(var firstname: String,
-             var surname: String = "markovic",
-             var age: Int = 22,
-){
-    open fun printInfo(){
+open class Person(
+    var firstname: String,
+    var surname: String = "markovic",
+    var age: Int = 22, open var gender: String = "undisclosed",
+) {
+    open fun printInfo() {
         println("First name: ${firstname} Surname: ${surname} age: ${age}")
     }
 
 }
 
-class Woman : Person{
+class Woman : Person {
     constructor(firstname: String) : super(firstname)
-    override fun printInfo(){
-        println("Woman override function: ${firstname}, ${surname}, ${age}")
+    override var gender = "female"
+    override fun printInfo() {
+        println("Woman override function: ${firstname}, ${surname}, ${age}, ${gender}")
     }
 }
