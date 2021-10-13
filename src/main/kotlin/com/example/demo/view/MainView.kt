f689dc12a7ab9868bd031f24982dcc0dd815be99
Jana Markovikj <j.markovic@artemisoft.mk>
2021-10-13 13:28:32 +0200

    start override

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -3,10 +3,11 @@ package com.example.demo.view
 import com.example.demo.app.Styles
 import tornadofx.*
 
-class MainView : View("Hello TornadoFX") {
+class MainView : View("TornadoFX App") {
     override val root = hbox {
         label(title) {
             addClass(Styles.heading)
         }
+        label("Hello")
     }
 }
