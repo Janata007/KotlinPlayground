6db46623c674f514442eb3c579b3f96e692fce27
Jana Markovikj <j.markovic@artemisoft.mk>
2021-10-13 13:42:18 +0200

    bind

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -1,13 +1,27 @@
 package com.example.demo.view
 
 import com.example.demo.app.Styles
+import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Pos
 import tornadofx.*
 
 class MainView : View("TornadoFX App") {
-    override val root = hbox {
-        label(title) {
+    var labelText = SimpleStringProperty()
+
+    override val root = vbox {
+        alignment = Pos.CENTER
+        spacing = 10.0
+        label(labelText) {
+            bind(labelText)
             addClass(Styles.heading)
         }
-        label("Hello")
+
+        button {
+            this.text = "Click"
+            action {
+                labelText.set("Hello")
+            }
+        }
+
     }
 }
